3ec0586bc057c91a15afdcadc39026e4b5198804
geekabhinav007 <abhinav26january@gmail.com>
2023-02-13 20:18:38 +0530

    added some randamization

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var elem = document.body;
 var two = new Two(params).appendTo(elem);
 
 // An array of possible shapes and colors to choose from
-var shapes = ['circle', 'rectangle', 'triangle'];
+var shapes = ['circle', 'rectangle', 'triangle', `pentagon`, `ploygon`, `star`, `heart`];
 var colors = [  'AliceBlue', 'AntiqueWhite', 'Aqua', 'Aquamarine', 'Azure', 'Beige', 'Bisque', 'Black',  'BlanchedAlmond', 'Blue', 'BlueViolet', 'Brown', 'BurlyWood', 'CadetBlue', 'Chartreuse',  'Chocolate', 'Coral', 'CornflowerBlue', 'Cornsilk', 'Crimson', 'Cyan', 'DarkBlue', 'DarkCyan',  'DarkGoldenRod', 'DarkGray', 'DarkGrey', 'DarkGreen', 'DarkKhaki', 'DarkMagenta', 'DarkOliveGreen',  'DarkOrange', 'DarkOrchid', 'DarkRed', 'DarkSalmon', 'DarkSeaGreen', 'DarkSlateBlue', 'DarkSlateGray',  'DarkSlateGrey', 'DarkTurquoise', 'DarkViolet', 'DeepPink', 'DeepSkyBlue', 'DimGray', 'DimGrey',  'DodgerBlue', 'FireBrick', 'FloralWhite', 'ForestGreen', 'Fuchsia', 'Gainsboro', 'GhostWhite',  'Gold', 'GoldenRod', 'Gray', 'Grey', 'Green', 'GreenYellow', 'HoneyDew', 'HotPink', 'IndianRed',  'Indigo', 'Ivory', 'Khaki', 'Lavender', 'LavenderBlush', 'LawnGreen', 'LemonChiffon', 'LightBlue',  'LightCoral', 'LightCyan', 'LightGoldenRodYellow', 'LightGray', 'LightGrey', 'LightGreen', 'LightPink',  'LightSalmon', 'LightSeaGreen', 'LightSkyBlue', 'LightSlateGray', 'LightSlateGrey', 'LightSteelBlue',  'LightYellow', 'Lime', 'LimeGreen', 'Linen', 'Magenta', 'Maroon', 'MediumAquaMarine', 'MediumBlue',  'MediumOrchid', 'MediumPurple', 'MediumSeaGreen', 'MediumSlateBlue', 'MediumSpringGreen', 'MediumTurquoise',  'MediumVioletRed', 'MidnightBlue', 'MintCream', 'MistyRose', 'Moccasin', 'NavajoWhite', 'Navy',  'OldLace', 'Olive', 'OliveDrab', 'Orange', 'OrangeRed', 'Orchid', 'PaleGoldenRod', 'PaleGreen',  'PaleTurquoise', 'PaleVioletRed', 'PapayaWhip', 'PeachPuff', 'Peru', 'Pink', 'Plum', 'PowderBlue',  'Purple', 'Red', 'RosyBrown', 'RoyalBlue', 'SaddleBrown', 'Salmon', 'SandyBrown', 'SeaGreen',  'SeaShell', 'Sienna', 'Silver', 'SkyBlue', 'SlateBlue', 'SlateGray', 'SlateGrey', 'Snow',  'SpringGreen', 'SteelBlue', 'Tan', 'Teal', 'Thistle', 'Tomato', 'Turquoise', 'Violet',  'Wheat', 'White', 'WhiteSmoke', 'Yellow', 'YellowGreen'];
 
 
@@ -35,9 +35,9 @@ function getRandomPosition() {
   return randomInRange(0, 1800);
 }
 
-// Returns a random time between 2000 and 5000 milliseconds
+// Returns a random time between 100 and 800 milliseconds
 function getRandomTime() {
-  return randomInRange(500, 800);
+  return randomInRange(100, 800);
 }
 
 // Draws a shape on the canvas with a random size, position, color, and shape type
@@ -57,15 +57,32 @@ function drawShape() {
     var rectangle = two.makeRectangle(x, y, size, size);
     rectangle.fill = color;
   } else if (shape === 'triangle') {
-    var triangle = two.makePolygon(x, y, size, y+x);
+    var triangle = two.makePolygon(x, y, size, 3);
     triangle.fill = color;
+  }else if (shape === 'pentagon') {
+    var pentagon = two.makePolygon(x, y, size, 5);
+    pentagon.fill = color;
+  }else if (shape === 'ploygon') {
+    var ploygon = two.makePolygon(x, y, size, 6);
+    ploygon.fill = color;
+  }else if (shape === 'star') {
+    var star = two.makeStar(x, y, size, 5, 0.5);
+    star.fill = color;
+  }else if (shape === 'heart') {
+    var heart = two.makeHeart(x, y, size);
+    heart.fill = color;
   }
+  
 
   // Update the canvas with the new shape
   two.update();
 
   // Call the drawShape function again after a random amount of time
   setTimeout(drawShape, time);
+  if(time > getRandomTime() && time < getRandomTime()+50) {
+    two.clear();
+    setTimeout(drawShape, time);
+  }
 }
 
 // Call the drawShape function for the first time to start the animation loop
